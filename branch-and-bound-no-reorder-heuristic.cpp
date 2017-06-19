2d84fd3bda275b36964eb417be2fd7f4dc568240
mseefelder <saa.marcos@gmail.com>
2017-06-19 11:33:21 -0300

    Implemented simple GRASP heuristic.

--- branch-and-bound-no-reorder-heuristic.cpp
+++ branch-and-bound-no-reorder-heuristic.cpp
@@ -3,6 +3,8 @@
 #include <forward_list>
 #include <vector>
 #include <queue>
+#include <stack>
+#include <unordered_set>
 #include <utility>
 #include <algorithm>
 #include <chrono>
@@ -34,7 +36,7 @@ int Backtrack(Graph &G, MBVSolutionUndo &sol, int &minimumSol, auto &start, long
 
 			if(sol.getEdgeState(e) == 0) {
 				if(sol.activateEdge(e)) {
-                    minimumSol = Backtrack(G, sol, minimumSol, start, pruneCount);
+										minimumSol = Backtrack(G, sol, minimumSol, start, pruneCount);
 					sol.undoActivateEdge();
 				}
 
@@ -62,18 +64,18 @@ int Backtrack(Graph &G, MBVSolutionUndo &sol, int &minimumSol, auto &start, long
 					minimumSol = sol.getBranchVertexCount();
 				}
 
-                //Check if we should branch
-                bool doBranch = (sol.getRelaxedSolution() < minimumSol) && (sol.getActiveEdgeCount() == G.nVertices-1);
-                if(!doBranch)
-                    pruneCount++;
+								//Check if we should branch
+								bool doBranch = (sol.getRelaxedSolution() < minimumSol) && (sol.getActiveEdgeCount() == G.nVertices-1);
+								if(!doBranch)
+										pruneCount++;
 
 				for (int x = 0; x < opcount; ++x) {
 					sol.undoActivateEdge();
 				}
 
 				//Only branch if relaxed bound is < minimumSol
-                if(doBranch)
-                    minimumSol = Backtrack(G, sol, minimumSol, start, pruneCount);
+								if(doBranch)
+										minimumSol = Backtrack(G, sol, minimumSol, start, pruneCount);
 
 				sol.undoProhibitEdge(e);
 				//sol.sortEdges();
@@ -87,6 +89,64 @@ int Backtrack(Graph &G, MBVSolutionUndo &sol, int &minimumSol, auto &start, long
 	return minimumSol;
 }
 
+void DFS (Graph &G, unordered_set<int>* neig, unordered_set<int> &result, int source, int destination) {
+
+	//Auxiliary queue used during the algorithm
+	std::stack<int> lifo;
+	//For each vertex: store its parent
+	int* parentEdge;
+	parentEdge = new int[G.nVertices];
+	//For each vertex: store its level
+	int* levels;
+	levels = new int[G.nVertices];
+	//Fill arrays
+	for (int i  = 0; i<G.nVertices; i++){
+		parentEdge[i]=-2;
+		levels[i]=-1;
+	}
+	//Set initial values
+	parentEdge[source]=-1;
+	levels[source]=0;
+	lifo.push(source);
+
+	int current = 0;
+	//int iterations = 0;
+	bool deleteFlag;
+	int n = 0;
+	while(!lifo.empty()){
+		current = lifo.top();
+		lifo.pop();
+		//iterations = neig[current].size();
+		//for ( int i = 0; i < iterations; i++ ){
+		for (int edge : neig[current]) {
+			//edge = neig[current][i];
+			n = (G.edges[edge].first == current) ? G.edges[edge].second : G.edges[edge].first; 
+			if (n == destination) {
+				parentEdge[destination]=edge;
+				levels[destination]=levels[current]+1;
+				break;
+			}
+			else if (parentEdge[n] == -2) {
+				lifo.push(n);
+				parentEdge[n]=edge;
+				levels[n]=levels[current]+1;
+			}
+		}
+	}
+
+	current = destination;
+	int iterations = levels[destination];
+	for (int i = 0; i < iterations; i++) {
+		result.insert(parentEdge[current]);
+
+		current = (G.edges[parentEdge[current]].first == current) ? G.edges[parentEdge[current]].second : G.edges[parentEdge[current]].first ;
+	}
+
+	delete [] parentEdge; delete [] levels;
+
+	return;
+}
+
 int MBVGrasp (Graph &G, int maxIter, int randomMargin) {
 	// Solution
 	int minBranchVertexCount = numeric_limits<int>::max();
@@ -94,7 +154,7 @@ int MBVGrasp (Graph &G, int maxIter, int randomMargin) {
 	// Random setup
 	std::random_device r;
 	std::default_random_engine engine(r());
-	std::uniform_int_distribution<int> uDist(0, randomMargin);
+	std::uniform_int_distribution<int> uDist(0, 0);
 
 	// Create solution
 	MBVSolutionUndo gS(G);
@@ -102,14 +162,14 @@ int MBVGrasp (Graph &G, int maxIter, int randomMargin) {
 	// Make edges connecting vertices with degree 1 obligatory
 	// Number of obligatory edges
 	int obl = 0;
-	for (int i = 0; i < G.nVertices; ++i)
-	{
-		if (G.vertexDegrees[i] == 1)
-		{
-			gS.activateEdge(G.vertices[i].front());
-			obl++;
-		}
-	}
+	//for (int i = 0; i < G.nVertices; ++i)
+	//{
+	//	if (G.vertexDegrees[i] == 1)
+	//	{
+	//		gS.activateEdge(G.vertices[i].front());
+	//		obl++;
+	//	}
+	//}
 
 	// Run maxIter iterations
 	for (int iter = 0; iter < maxIter; ++iter)
@@ -126,14 +186,16 @@ int MBVGrasp (Graph &G, int maxIter, int randomMargin) {
 		queue<int> cycleEdges;
 
 		// Auxiliar structure for local search (represents the tree)
-		int parent[G.nVertices];
+		//int parent[G.nVertices];
+		//int rank[G.nVertices];
 		int treeDegrees[G.nVertices];
-		int rank[G.nVertices];
+		unordered_set<int> neig[G.nVertices]; /// Fill this
 		for (int i = 0; i < G.nVertices; ++i)
 		{
-			parent[i] = -1;
 			treeDegrees[i] = gS.vertexDegrees[i];
-			rank[i] = 0;
+			
+			//parent[i] = -1;
+			//rank[i] = 0;
 		}
 
 		// Choose semi-randomly (randomMargin) with Kruskal
@@ -157,15 +219,18 @@ int MBVGrasp (Graph &G, int maxIter, int randomMargin) {
 				v = G.edges[e].second;
 				treeDegrees[u]++;
 				treeDegrees[v]++;
+				// Update neig array
+				neig[u].insert(e);
+				neig[v].insert(e);
 				// Add edge to auxiliar structure for local search (in the form of a parent relation)
-				if(treeDegrees[v] > treeDegrees[u]) {
-					parent[u] = v;
-					rank[u] = rank[v]+1;
-				}
-				else {
-					parent[v] = u;
-					rank[v] = rank[u]+1;
-				}
+				//if(treeDegrees[v] > treeDegrees[u]) {
+				//	parent[u] = v;
+				//	rank[u] = rank[v]+1;
+				//}
+				//else {
+				//	parent[v] = u;
+				//	rank[v] = rank[u]+1;
+				//}
 			}
 
 			// Remove edge from vector of edges left
@@ -183,13 +248,12 @@ int MBVGrasp (Graph &G, int maxIter, int randomMargin) {
 
 		// Current branch vertex count
 		int branchVertexCount = gS.getBranchVertexCount(); /// ALWAYS UPDATE THIS WHEN UPDATING TREE
-		cout<<branchVertexCount<<endl;
 
-		/**
+		/**/
 		// Add edges left from edges vector to cycleEdges vector
 		while(!edges.empty()) {
-		    cycleEdges.push(edges.back());
-		    edges.pop_back();
+				cycleEdges.push(edges.back());
+				edges.pop_back();
 		}
 
 		// Number of iterations that will be executed to look for improvement
@@ -200,6 +264,7 @@ int MBVGrasp (Graph &G, int maxIter, int randomMargin) {
 		// Local search starting on gS
 		// Iterate over edges to evaluate local search neighbourhood
 		e = 0, u = 0, v = 0;
+		int uLine = 0, vLine = 0;
 		while(remaining!=0) {
 			// Remove last element from vector
 			e = cycleEdges.front();
@@ -216,24 +281,53 @@ int MBVGrasp (Graph &G, int maxIter, int randomMargin) {
 				treeDegrees[u]++;
 				treeDegrees[v]++;
 				
-				/// Get sequence SEQ of edges in cycle C
-				
+				/// Get sequence SEQ of edges in cycle C (some kind of 'vector<int> DFS(vector<int>[] neig, u, v, size)' function)
+				unordered_set<int> seq;
+				DFS(G, neig, seq, u, v);
+
+				//cout<<":: "<<u<<" --- "<<v<<endl;
+				//for (int dummy : seq) cout<<G.edges[dummy].first<<","<<G.edges[dummy].second<<" -> "; cout<<endl;
+
 				/// For each edge e'(i,j) on SEQ:
+				for (int eLine : seq){
+					uLine = G.edges[eLine].first;
+					vLine = G.edges[eLine].second;
 					/// If removing e' from tree reduces the degree of (i || j) from 3 to 2
+					if (treeDegrees[uLine] == 3 || treeDegrees[vLine] == 3) {
 						/// Remove e' from tree
+						neig[uLine].erase(eLine);
+						neig[vLine].erase(eLine);
+						if(treeDegrees[uLine]-- == 3) branchVertexCount--;
+						if(treeDegrees[vLine]-- == 3) branchVertexCount--;
 						/// Add e' to cycleEdges
+						cycleEdges.push(eLine);
 						/// (remaining = inserted+1)
+						remaining = inserted+1;
 						/// (inserted = 0)
+						inserted = 0;
 						/// Add e to tree, updating parent and rank arrays
+						neig[uLine].insert(eLine);
+						neig[vLine].insert(eLine);
 						/// Make sure e will stay in tree (keepChange = true)
+						keepChange = true;
 						/// Break
+						break;
+					}
+				}
 
 				/// If no improvement was encountered (keepChange == false):
-					/// Undo adding e to tree 
+				if (!keepChange) {
+					/// Undo adding e to tree
+					treeDegrees[u]--;
+					treeDegrees[v]--; 
 					/// Add e to cycleEdges
+					cycleEdges.push(e);
 					/// (inserted++)
+					inserted++;
+				}
 			}
 			/// (remaining--)
+			remaining--;
 			
 		}
 		/**/
@@ -245,13 +339,11 @@ int MBVGrasp (Graph &G, int maxIter, int randomMargin) {
 			/// Undo nVertices - 1 - obl activations
 			gS.undoActivateEdge();
 		}
+
+		uDist.param(uniform_int_distribution<int>::param_type(0, (iter>randomMargin) ? randomMargin : iter));
 	}
 
 	return minBranchVertexCount;
-}
-
-int randomGreedy() {
-
 }
 
 int main(int argc, char const *argv[])
@@ -269,7 +361,7 @@ int main(int argc, char const *argv[])
 		G.addEdge(a-1, b-1);
 	}
 
-	int heuristicResult = MBVGrasp (G, 30, 15);
+	int heuristicResult = MBVGrasp (G, (int)(mEdges*0.8), (int)(mEdges*0.75));
 	cout<<"Best heuristic result: "<<heuristicResult<<endl;
 	
 	/**
@@ -293,13 +385,13 @@ int main(int argc, char const *argv[])
 		e = initialKruskal.edgeIndex[i];
 		bool activated = initialKruskal.activateEdge(e);
 
-        cout<<e<<": "<<initialKruskal.edgeWeight[e]<<"(";
-        cout<<G.vertexDegrees[G.edges[e].first]<<","<<G.vertexDegrees[G.edges[e].second];
-        cout<<") "<<activated<<endl;
+				cout<<e<<": "<<initialKruskal.edgeWeight[e]<<"(";
+				cout<<G.vertexDegrees[G.edges[e].first]<<","<<G.vertexDegrees[G.edges[e].second];
+				cout<<") "<<activated<<endl;
 
 		if(initialKruskal.getActiveEdgeCount() == nVertices - 1 ){
-            cout<<endl;
-            cout<<"Relaxed solution "<<initialKruskal.getRelaxedSolution()<<endl;
+						cout<<endl;
+						cout<<"Relaxed solution "<<initialKruskal.getRelaxedSolution()<<endl;
 			break;
 		}
 	}
@@ -307,19 +399,19 @@ int main(int argc, char const *argv[])
 	minimumSol = initialKruskal.getBranchVertexCount();
 	cout<<"Initial heuristic: "<<minimumSol<<endl;
 	/*
-    for (int i = 0; i < nVertices-1; i++) {
-        initialKruskal.undoActivateEdge();
-    }
-    cout<<"Relaxed solution "<<initialKruskal.getRelaxedSolution()<<endl;
+		for (int i = 0; i < nVertices-1; i++) {
+				initialKruskal.undoActivateEdge();
+		}
+		cout<<"Relaxed solution "<<initialKruskal.getRelaxedSolution()<<endl;
 
-    long long pruneCount = 0;
+		long long pruneCount = 0;
 
 	auto start = chrono::steady_clock::now();
 	minimumSol = Backtrack(G, S, minimumSol, start, pruneCount);
 	auto end = chrono::steady_clock::now();
 	cout << "Solution has " << minimumSol << " branch vertices! (";
 	cout << chrono::duration <double, milli> (end-start).count() << " ms), ";
-    cout << pruneCount << " prunes." << endl;
+		cout << pruneCount << " prunes." << endl;
 	*/
 	return 0;
 }
---
+++ mbv-utils.h
@@ -0,0 +1,413 @@
+#include <limits>
+#include <iostream>
+#include <forward_list>
+#include <utility>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+
+struct UnionOperation{
+	int parent;
+	int child;
+	bool increasedParentRank;
+
+	UnionOperation(): parent(0), child(0), increasedParentRank(false){}
+	UnionOperation(int p, int c, bool i): parent(p), child(c), increasedParentRank(i){}
+};
+
+class UnionFind{
+private:
+	int* parent;
+	int* rank;
+	int elementCount;
+	int disjointSetCount;
+
+	int findSet (int x) {
+		int top = x;
+
+		while(parent[top] != top) {
+			top = parent[top];
+		}
+
+		return top;
+	}
+
+public:
+	UnionFind() : parent(nullptr), elementCount(0), disjointSetCount(0) {	}
+
+	UnionFind(int n) : elementCount(n), disjointSetCount(n) {
+		parent = new int[elementCount];
+		rank = new int[elementCount];
+		for (int i = 0; i < elementCount; ++i)
+		{
+			parent[i] = i;
+			rank[i] = 0;
+		}
+		
+	}
+
+	~UnionFind() {
+		if(parent){
+			delete [] parent;
+		}
+		if (rank)
+		{
+			delete [] rank;
+		}
+	}
+
+	void printElements() {
+		for (int i = 0; i < elementCount; ++i)
+		{
+			cout<<parent[i]<<",";
+		}
+		cout<<endl;
+	}
+
+	bool unionSet (int x, int y, UnionOperation &op) {
+		int setX = findSet(x);
+		int setY = findSet(y);
+		if(setX == setY) {
+			return false;
+		}
+		
+		if(rank[setX] > parent[setY]){
+			parent[setY] = setX;
+			op = UnionOperation(setX,setY,false);
+		}
+		else{
+			parent[setX] = setY;
+			rank[setY]++;
+			op = UnionOperation(setY,setX,true);
+		}
+		
+		disjointSetCount--;
+		return true;
+	}
+
+	void undoUnion(UnionOperation &op) {
+		parent[op.child] = op.child;
+		if(op.increasedParentRank){
+			rank[op.parent]--;
+		}
+		disjointSetCount++;
+		return;
+	}
+
+	int getElementCount() {
+		return elementCount;
+	}
+
+	int getElement(int e) {
+		return parent[e];
+	}
+
+	int getDisjointSetCount() {
+		printElements();
+		return disjointSetCount;
+	}
+};
+
+class Graph {
+public:
+	//For every vertex, a forward_list of edges it's connected to
+	forward_list<int>* vertices;
+	//A list with all the edges
+	pair<int, int>* edges;
+	//A list with vertex degrees
+	int* vertexDegrees;
+	//Sizes
+	int nVertices, mEdges;
+	//Amount of edges already added
+	int addedEdges;
+
+	Graph() : vertices(nullptr), edges(nullptr), vertexDegrees(nullptr), nVertices(0), mEdges(0), addedEdges(0) {}
+
+	Graph(int n, int m) : nVertices(n), mEdges(m), addedEdges(0) {
+		vertices = new forward_list<int>[nVertices];
+		vertexDegrees = new int[nVertices];
+		edges = new pair<int, int>[mEdges];
+
+		for (int i = 0; i < nVertices; ++i)
+		{
+			vertexDegrees[i] = 0;
+		}
+	} 
+
+	void addEdge(int u, int v) {
+		if(addedEdges == mEdges) {
+			cout<<"Already added all edges"<<endl;
+		}
+		edges[addedEdges] = pair<int, int>(u, v);
+		vertices[u].push_front(addedEdges);
+		vertexDegrees[u]++;
+		vertices[v].push_front(addedEdges);
+		vertexDegrees[v]++;
+		addedEdges++;
+	}
+
+};
+
+class sort_indices
+{
+   private:
+     float* valueArr;
+   public:
+     sort_indices(float* arr) : valueArr(arr) {}
+     bool operator()(int i, int j) const { return valueArr[i]<valueArr[j]; }
+};
+
+class MBVSolutionUndo {
+private:
+
+public:
+	int branchVertexCount, activeEdgeCount;
+	UnionFind* vertexConnected;
+	int* edgeState;
+	float* edgeWeight;
+	int* edgeIndex;
+	int* vertexDegrees;
+	int* vertexProhibitedDegrees;
+	UnionOperation* unionsList;
+	int unionCounter;
+	int* unionEdges;
+	Graph *G;
+    float* relaxedSolution;
+    int currentRelaxedSolution;
+    float constantRelaxedSubtractor;
+
+	MBVSolutionUndo(Graph &sourceGraph) : 
+	G(&sourceGraph), branchVertexCount(0), activeEdgeCount(0), unionCounter(0), currentRelaxedSolution(0), constantRelaxedSubtractor(0.0) {
+		vertexDegrees = new int[G->nVertices];
+		vertexProhibitedDegrees = new int[G->nVertices];
+		vertexConnected = new UnionFind(G->nVertices);
+		edgeState = new int[G->mEdges];
+		edgeWeight = new float[G->mEdges];
+		edgeIndex = new int[G->mEdges];
+		unionsList = new UnionOperation[G->nVertices-1];
+		unionEdges = new int[G->nVertices-1];
+        relaxedSolution = new float[G->nVertices];
+
+		int deg = 0;
+
+		for (int v = 0; v < G->nVertices; ++v)
+		{
+			vertexDegrees[v] = 0;
+			vertexProhibitedDegrees[v] = 0;
+            relaxedSolution[v] = 0.0;
+		}
+
+		for (int e = 0; e < G->mEdges; ++e)
+		{
+			edgeState[e] = 0;
+			edgeWeight[e] = 0.0;
+			edgeIndex[e] = e;
+		}
+
+		//sort
+		sortEdges();
+
+		cout<<"Sorted!"<<endl;
+	}
+
+	MBVSolutionUndo(int bvc) : G(nullptr), branchVertexCount(bvc), activeEdgeCount(0) {}
+
+	~MBVSolutionUndo() {
+		/**/
+		//if (vertexConnected)
+		//{
+		//	delete vertexConnected;
+		//}
+		if (edgeState)
+		{
+			delete [] edgeState;
+		}
+		if (vertexDegrees)
+		{
+			delete [] vertexDegrees;
+		}
+		if (edgeWeight)
+		{
+			delete [] edgeWeight;
+		}
+		if (edgeIndex)
+		{
+			delete [] edgeIndex;
+		}
+		/**/
+	}
+
+	void sortEdges() {
+		int deg = 0;
+		int v = 0;
+		//Calculate edge weights
+        for (int e = 0; e < G->mEdges; ++e)
+		{
+			edgeIndex[e] = e;
+            edgeWeight[e] = 0.0;
+
+			//Prepare "heuristic" weights for sorting
+			v = G->edges[e].first;
+			deg = G->vertexDegrees[v] - vertexProhibitedDegrees[v];
+			if(deg > 2) {
+				edgeWeight[e] += 1.0/deg;
+			}
+
+			v = G->edges[e].second;
+			deg = G->vertexDegrees[v] - vertexProhibitedDegrees[v];
+			if(deg > 2) {
+				edgeWeight[e] += 1.0/deg;
+			}
+		}
+        //Calculate constant weight subtractor
+        constantRelaxedSubtractor = 0.0;
+        for (int v = 0; v < G->nVertices; ++v)
+        {
+            deg = G->vertexDegrees[v] - vertexProhibitedDegrees[v];
+			if(deg > 2) {
+				constantRelaxedSubtractor += 2.0/deg;
+			} 
+        }
+
+		//sort
+		sort(edgeIndex, edgeIndex+G->mEdges, sort_indices(edgeWeight));
+	}
+
+	int getEdgeState(int e) {
+		return edgeState[e];
+	}
+
+	bool activateEdge(int e) {	
+		if(edgeState[e] != 0) {
+			return false;
+		}	
+		//Vertices connected by edge
+		int u = G->edges[e].first;
+		int v = G->edges[e].second;
+		UnionOperation op;
+
+		bool doesntMakeCycle = vertexConnected->unionSet(u, v, op);
+		
+		//If this forms a cycle end with false
+		if(!doesntMakeCycle){
+			return false;
+		}
+
+		//store last union
+		unionsList[unionCounter] = op;
+		unionEdges[unionCounter] = e;
+		unionCounter++;
+
+		//activate edge
+		edgeState[e] = 1;
+		activeEdgeCount++;
+        relaxedSolution[currentRelaxedSolution + 1] = relaxedSolution[currentRelaxedSolution] + edgeWeight[e];
+        currentRelaxedSolution++;
+
+		//Update branch vertice count
+		if(vertexDegrees[u] == 2) {
+			branchVertexCount++;
+		}
+		if(vertexDegrees[v] == 2) {
+			branchVertexCount++;
+		}
+
+		//Increase vertices' degrees
+		vertexDegrees[u]++; vertexDegrees[v]++;
+
+		/*
+		cout<<"  activate [";
+		for (int i = 0; i < G->mEdges; i++) {
+			cout<<i<<": "<<edgeState[i]<<", ";
+		}
+		cout<<"]"<<endl;
+		*/
+
+		return true;
+	}
+
+	void undoActivateEdge() {
+		unionCounter--;
+
+		vertexConnected->undoUnion(unionsList[unionCounter]);
+
+		//deactivate edge
+		int e = unionEdges[unionCounter];
+		edgeState[e] = 0;
+		activeEdgeCount--;
+        currentRelaxedSolution--;
+
+		//Decrease vertices' degrees
+		//Vertices connected by edge
+		int u = G->edges[e].first;
+		int v = G->edges[e].second;
+		vertexDegrees[u]--; vertexDegrees[v]--;
+
+		//Update branch vertice count
+		if(vertexDegrees[u] == 2) {
+			branchVertexCount--;
+		}
+		if(vertexDegrees[v] == 2) {
+			branchVertexCount--;
+		}	
+
+		/*
+		cout<<"deactivate [";
+		for (int i = 0; i < G->mEdges; i++) {
+			cout<<i<<": "<<edgeState[i]<<", ";
+		}
+		cout<<"]"<<endl;	
+		*/
+	}
+
+	void prohibitEdge(int e) {
+		//Vertices connected by edge
+		int u = G->edges[e].first;
+		int v = G->edges[e].second;
+		vertexProhibitedDegrees[u]++;
+		vertexProhibitedDegrees[v]++;
+		edgeState[e] = -1;
+
+		/*
+		cout<<"  prohibit [";
+		for (int i = 0; i < G->mEdges; i++) {
+			cout<<i<<": "<<edgeState[i]<<", ";
+		}
+		cout<<"]"<<endl;
+		*/
+	}
+
+	void undoProhibitEdge(int e) {
+		//Vertices connected by edge
+		int u = G->edges[e].first;
+		int v = G->edges[e].second;
+		vertexProhibitedDegrees[u]--;
+		vertexProhibitedDegrees[v]--;
+		edgeState[e] = 0;
+
+		/*
+		cout<<"unprohibit [";
+		for (int i = 0; i < G->mEdges; i++) {
+			cout<<i<<": "<<edgeState[i]<<", ";
+		}
+		cout<<"]"<<endl;
+		*/
+	}
+
+	int getBranchVertexCount() {
+		return branchVertexCount;
+	}
+
+	int getActiveEdgeCount() {
+		return activeEdgeCount;
+	}
+
+	int getDisjointSetCount() {
+		return vertexConnected->getDisjointSetCount();
+	}
+
+    float getRelaxedSolution() {
+        return relaxedSolution[currentRelaxedSolution] - constantRelaxedSubtractor;
+    }
+};
